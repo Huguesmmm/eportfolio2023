98d46c427cf604156f601c7fcea8a54da6cb61d6
Hugues Prud'homme <70539777+Huguesmmm@users.noreply.github.com>
2023-11-05 23:16:21 -0500

    added animated blob section

---
+++ src/components/Outspace.tsx
@@ -0,0 +1,10 @@
+import Spline from '@splinetool/react-spline';
+
+export default function Outspace() {
+    return (
+        <div className='h-screen'>
+            <Spline scene="https://prod.spline.design/OrWIV6jINkU6p3mz/scene.splinecode" />
+        </div>
+        
+    );
+}
